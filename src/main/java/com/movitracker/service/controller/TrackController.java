3e3efb295f0b0b32e649c3aa02119f180406ccd9
Dmitry Raguzin <draguzin@aligntech.com>
2018-12-15 16:02:33 +0300

    Just testing heroku autodeploy from github

--- src/main/java/com/movitracker/service/controller/TrackController.java
+++ src/main/java/com/movitracker/service/controller/TrackController.java
@@ -1,13 +1,14 @@
 package com.movitracker.service.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TrackController {
 
     @GetMapping(path = "/hello")
-    public String hello() {
-        return "hello world";
+    public String hello(@RequestParam String name) {
+        return "hello " + name;
     }
 }
